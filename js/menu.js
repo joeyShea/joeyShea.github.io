9edce166fea30fc744d8774e957643d99123bc62
joeyShea <27josephs@gmail.com>
2024-09-05 12:11:51 -0500

    restructured pages

--- js/menu.js
+++ js/menu.js
@@ -1,47 +1,58 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 	//if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
 	var MQL = 1170;
 
 	//primary navigation slide-in effect
-	if($(window).width() > MQL) {
+	if ($(window).width() > MQL) {
 		var headerHeight = $('.box-header').height();
 		$(window).on('scroll',
-		{
-	        previousTop: 0
-	    }, 
-	    function () {
-		    var currentTop = $(window).scrollTop();
-		    //check if user is scrolling up
-		    if (currentTop < this.previousTop ) {
-		    	//if scrolling up...
-		    	if (currentTop > 0 && $('.box-header').hasClass('is-fixed')) {
-		    		$('.box-header').addClass('is-visible');
-		    	} else {
-		    		$('.box-header').removeClass('is-visible is-fixed');
-		    	}
-		    } else {
-		    	//if scrolling down...
-		    	$('.box-header').removeClass('is-visible');
-		    	if( currentTop > headerHeight && !$('.box-header').hasClass('is-fixed')) $('.box-header').addClass('is-fixed');
-		    }
-		    this.previousTop = currentTop;
-		});
+			{
+				previousTop: 0
+			},
+			function () {
+				var currentTop = $(window).scrollTop();
+				//check if user is scrolling up
+				if (currentTop < this.previousTop) {
+					//if scrolling up...
+					if (currentTop > 0 && $('.box-header').hasClass('is-fixed')) {
+						$('.box-header').addClass('is-visible');
+					} else {
+						$('.box-header').removeClass('is-visible is-fixed');
+					}
+				} else {
+					//if scrolling down...
+					$('.box-header').removeClass('is-visible');
+					if (currentTop > headerHeight && !$('.box-header').hasClass('is-fixed')) $('.box-header').addClass('is-fixed');
+				}
+				this.previousTop = currentTop;
+			});
 	}
 
-	//open/close primary navigation
-	$('.box-primary-nav-trigger').on('click', function(){
-		$('.box-menu-icon').toggleClass('is-clicked'); 
+	// Function to toggle menu
+	function toggleMenu() {
+		$('.box-menu-icon').toggleClass('is-clicked');
 		$('.box-header').toggleClass('menu-is-open');
-		
-		//in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
-		if( $('.box-primary-nav').hasClass('is-visible') ) {
-			$('.box-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+
+		// Firefox transitions fix
+		if ($('.box-primary-nav').hasClass('is-visible')) {
+			$('.box-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
 				$('body').removeClass('overflow-hidden');
 			});
 		} else {
-			$('.box-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+			$('.box-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
 				$('body').addClass('overflow-hidden');
-			});	
+			});
 		}
+	}
+
+	// Open/close primary navigation
+	$('.box-primary-nav-trigger').on('click', function () {
+		toggleMenu();
 	});
+
+	// Open/close primary navigation with the same-page ID element
+	$('#same-page').on('click', function () {
+		toggleMenu();
+	});
+
 });
