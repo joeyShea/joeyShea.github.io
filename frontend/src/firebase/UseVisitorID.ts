827dfbab9b0547a9dd783712db45cdb389e09a3f
joeyShea <27josephs@gmail.com>
2025-03-19 19:18:15 -0500

    Tweat

--- frontend/src/firebase/UseVisitorID.ts
+++ frontend/src/firebase/UseVisitorID.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function useVisitorID() {
   const initialID = sessionStorage.getItem("visitorID") || crypto.randomUUID();
-  const [visitorID, setVisitorID] = useState(initialID);
+  const [visitorID] = useState(initialID);
 
   useEffect(() => {
     sessionStorage.setItem("visitorID", visitorID);
